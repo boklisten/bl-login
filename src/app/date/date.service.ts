c828892f1c9081e912c801d5f5fd5b23f36e473a
holskil <aholskil@gmail.com>
2018-03-02 18:50:06 +0100

    feat: DateService

---
+++ src/app/date/date.service.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core';
+import * as moment from 'moment';
+
+@Injectable()
+export class DateService {
+	
+	constructor() {
+	}
+	
+	isUnder18(dob: Date | string) {
+		if (!(dob instanceof Date)) {
+			dob = new Date(dob);
+		}
+		
+		const eighteenDate = moment().subtract(18, 'years');
+		
+		return (moment(dob).isAfter(eighteenDate));
+		
+	}
+	
+}
