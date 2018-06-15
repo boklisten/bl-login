4ba5d439eafcdb288adcb68af906f980c9057fc3
holskil <aholskil@gmail.com>
2018-06-15 15:46:31 +0200

    translate: added i18n tags in register folder

--- src/app/register/register-detail/register-detail.component.ts
+++ src/app/register/register-detail/register-detail.component.ts
@@ -13,11 +13,11 @@ import {DateService} from "../../date/date.service";
 
 @Injectable()
 export class NgbDateNativeAdapter extends NgbDateAdapter<Date> {
-	
+
 	fromModel(date: Date): NgbDateStruct {
 		return (date && date.getFullYear) ? {year: date.getFullYear(), month: date.getMonth() + 1, day: date.getDate()} : null;
 	}
-	
+
 	toModel(date: NgbDateStruct): Date {
 		return date ? new Date(date.year, date.month - 1, date.day) : null;
 	}
@@ -35,39 +35,29 @@ export class RegisterDetailComponent implements OnInit {
 	public registerDetailTitle: string;
 	public successMsg: string;
 	public warningMsg: string;
-	
+
 	public branchNotSelectedWarning: string;
-	
+
 	public defaultBranch: {name: string, id?: string};
 	public username: string;
-	public tooltipFullName: string;
-	public tooltipMobile: string;
-	public tooltipAddress: string;
-	public tooltipPostCode: string;
-	public tooltipPostCity: string;
-	public tooltipBirthday: string;
-	public tooltipBranch: string;
-	public tooltipSelectBranch: string;
-	public tooltipGuardianEmail: string;
 	public branches: any[];
 	private _defaultGroup: any;
 	private _userDetail: UserDetail;
-	public selectedBranch: Branch;
-	public userDetailsWasSavedMsg: string;
 	public userAgreementUrl: string;
-	
-	
+	public warning: boolean;
+
+
 	public registerForm: FormGroup;
-	
+
 	constructor(@Inject(FormBuilder) fb: FormBuilder, private _localLoginService: LocalLoginService,
 				private _registerDetailService: RegisterDetailService, private _authLoginService: AuthLoginService,
 				private _router: Router, private _route: ActivatedRoute, private _branchService: BranchService,
 				private _ngbDatepickerConfig: NgbDatepickerConfig, private _dateService: DateService) {
-		
-		
+
+
 		this._ngbDatepickerConfig.minDate = {year: 1890, month: 1, day: 1};
 		this._ngbDatepickerConfig.maxDate = {year: new Date().getFullYear(), month: new Date().getMonth() + 1, day: new Date().getDate()};
-		
+
 		this._defaultGroup = {
 			name: '',
 			phone: '',
@@ -82,41 +72,31 @@ export class RegisterDetailComponent implements OnInit {
 				email: ''
 			})
 		};
-		
+
+		this.warning = false;
+
 		this.registerForm = fb.group(this._defaultGroup);
-		
-		
-		
-		this.registerDetailTitle = 'Register your details';
-		this.branchNotSelectedWarning = 'You must select a branch to continue';
-		this.tooltipFullName = 'Full name';
-		this.tooltipMobile = 'Mobile number';
-		this.tooltipAddress = 'Address';
-		this.tooltipPostCode = 'code';
-		this.tooltipPostCity = 'city';
-		this.tooltipBirthday = 'Birthday';
-		this.tooltipBranch = 'Branch';
-		this.tooltipSelectBranch = 'Select Branch';
-		this.tooltipGuardianEmail = 'Guardian email';
-		this.userDetailsWasSavedMsg = 'Userdetails was saved';
+
+
+
 		this.userAgreementUrl = LOGIN_MODULE_SETTINGS.userAgreementUrl;
-		this.defaultBranch = {name: 'Select Branch', id: null};
+		this.defaultBranch = {name: 'Velg skole', id: null};
 	}
-	
+
 	ngOnInit() {
-		
+
 		if (!this._authLoginService.isLoggedIn()) {
 			this._router.navigate(['../../menu'], {relativeTo: this._route});
 		}
-		
+
 		this.fetchBranches();
 		this.fetchUserDetails();
 	}
-	
+
 	public showGuardianRegister(): boolean {
 		return this.isUnder18();
 	}
-	
+
 	private fetchBranches() {
 		this._branchService.get().then((branches: Branch[]) => {
 			this.branches = branches;
@@ -124,7 +104,7 @@ export class RegisterDetailComponent implements OnInit {
 			console.log('RegisterDetailComponent: could not get branches');
 		});
 	}
-	
+
 	private fetchUserDetails() {
 		this._registerDetailService.getUserDetails().then((userDetail: UserDetail) => {
 			this.setUserDetail(userDetail);
@@ -132,7 +112,7 @@ export class RegisterDetailComponent implements OnInit {
 			this._router.navigate(['../../menu'], {relativeTo: this._route});
 		});
 	}
-	
+
 	private setUserDetail(userDetail: UserDetail) {
 		this.registerForm.reset();
 		this._userDetail = userDetail;
@@ -147,42 +127,41 @@ export class RegisterDetailComponent implements OnInit {
 		this.registerForm.setValue(this._defaultGroup);
 		this.username = userDetail.email;
 	}
-	
+
 	private convertDateToNgbDatepicker(theDate: Date) {
 		return {year: theDate.getFullYear(), month: theDate.getMonth(), day: theDate.getDay()};
 	}
-	
+
 	public isUnder18(): boolean {
 		return this._dateService.isUnder18(this.registerForm.value.dob);
 	}
-	
+
 	public onUpdateDetails() {
-		
+
 		if (!this.registerForm.dirty) {
 			return;
 		}
-		
+
 		this._registerDetailService.updateDetails(this.registerForm.value).then((userDetail: UserDetail) => {
-			
 			this.setUserDetail(userDetail);
-			this.setSuccess(this.userDetailsWasSavedMsg);
+			this._router.navigateByUrl(LOGIN_MODULE_SETTINGS.successPath);
 		}).catch((blApiErr: BlApiError) => {
 			console.log('got error from server when updating the userDetail: ', blApiErr);
 		});
 	}
-	
+
 	private setSuccess(msg: string) {
 		this.successMsg = msg;
-		
+
 		setTimeout(() => {
 			this.successMsg = null;
 		}, 3000);
 	}
-	
+
 	private setWarning(msg: string) {
 		this.warningMsg = msg;
 	}
-	
+
 	public onDoItLater() {
 		this._router.navigateByUrl(LOGIN_MODULE_SETTINGS.successPath);
 	}
