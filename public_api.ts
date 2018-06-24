8925adee68f217051cbb03df8665bb5bc7c24362
holskil <aholskil@gmail.com>
2018-06-24 17:55:15 +0200

    fix: userDetailEditComponent exported in public_api.ts

--- public_api.ts
+++ public_api.ts
@@ -2,3 +2,4 @@
 export {LoginModule} from "./src/app/login/login.module";
 export * from './src/app/login/login.component';
 export {AuthLoginService} from './src/app/login/auth-login.service';
+export {UserDetailEditComponent} from './src/app/user-detail/user-detail-edit/user-detail-edit.component';
