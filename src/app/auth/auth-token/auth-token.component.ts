c1398137573e45c59e1404851effa0c574eed9c0
holskil <aholskil@gmail.com>
2018-08-20 19:02:14 +0200

    feat: added a spinner on token saving

--- src/app/auth/auth-token/auth-token.component.ts
+++ src/app/auth/auth-token/auth-token.component.ts
@@ -10,21 +10,29 @@ import {LOGIN_MODULE_SETTINGS} from "../../login/login-module-settings";
 	styleUrls: ['./auth-token.component.scss']
 })
 export class AuthTokenComponent implements OnInit {
-	
+	public addTokensError: boolean;
+
 	constructor(private _router: Router, private _route: ActivatedRoute, private _tokenService: TokenService,
 				private _authLoginService: AuthLoginService) {
 	}
-	
+
 	ngOnInit() {
 		this._route.paramMap.subscribe((paramMap: ParamMap) => {
 			if (!paramMap || !paramMap.get('accessToken') || !paramMap.get('refreshToken')) {
 				this._authLoginService.logout('auth/login');
 			} else {
-				this._tokenService.addAccessToken(paramMap.get('accessToken'));
-				this._tokenService.addRefreshToken(paramMap.get('refreshToken'));
-				this._authLoginService.login(LOGIN_MODULE_SETTINGS.successPath);
+				try {
+					this._tokenService.addAccessToken(paramMap.get('accessToken'));
+					this._tokenService.addRefreshToken(paramMap.get('refreshToken'));
+					this._authLoginService.login(LOGIN_MODULE_SETTINGS.successPath);
+				} catch (e) {
+					this.addTokensError = true;
+				}
 			}
 		});
-		
+	}
+
+	onLoginAgainClick() {
+		this._router.navigate(['auth/login']);
 	}
 }
