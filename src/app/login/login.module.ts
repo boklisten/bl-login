375594e6bcb7c5102388e0293e30cd64416d85e4
holskil <aholskil@gmail.com>
2019-01-13 10:17:27 +0100

    fix: settings.userDetailNotVaildPath to be set in login.module

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,48 +1,85 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {LoginComponent} from './login.component';
-import {AuthLoginService} from "./auth-login.service";
-import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
-import {LocalLoginService} from "./local-login/local-login.service";
-import { LocalLoginComponent } from './local-login/local-login.component';
-import {FormsModule} from "@angular/forms";
-import { SocialLoginComponent } from './social-login/social-login.component';
-import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
-import {LocalRegisterService} from "../register/local-register/local-register.service";
-import {LoginRoutingModule} from "./login-routing.module";
-import {RegisterModule} from "../register/register.module";
-import { LoginMenuComponent } from './login-menu/login-menu.component';
-import { LoginAuthComponent } from './login-auth/login-auth.component';
-import {BlConnectModule} from '@wizardcoder/bl-connect';
-import {SocialLoginService} from "./social-login/social-login.service";
-import { LoginSuccessComponent } from './login-success/login-success.component';
-import {LOGIN_MODULE_SETTINGS, LoginModuleSettings} from "./login-module-settings";
-import {AuthModule} from "../auth/auth.module";
-import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { LoginComponent } from "./login.component";
+import { AuthLoginService } from "./auth-login.service";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { LocalLoginService } from "./local-login/local-login.service";
+import { LocalLoginComponent } from "./local-login/local-login.component";
+import { FormsModule } from "@angular/forms";
+import { SocialLoginComponent } from "./social-login/social-login.component";
+import { ForgotPasswordComponent } from "./forgot-password/forgot-password.component";
+import { LocalRegisterService } from "../register/local-register/local-register.service";
+import { LoginRoutingModule } from "./login-routing.module";
+import { RegisterModule } from "../register/register.module";
+import { LoginMenuComponent } from "./login-menu/login-menu.component";
+import { LoginAuthComponent } from "./login-auth/login-auth.component";
+import { BlConnectModule } from "@wizardcoder/bl-connect";
+import { SocialLoginService } from "./social-login/social-login.service";
+import { LoginSuccessComponent } from "./login-success/login-success.component";
+import {
+	LOGIN_MODULE_SETTINGS,
+	LoginModuleSettings
+} from "./login-module-settings";
+import { AuthModule } from "../auth/auth.module";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
-import {library} from "@fortawesome/fontawesome-svg-core";
+import { library } from "@fortawesome/fontawesome-svg-core";
 
-import {faSquare} from "@fortawesome/free-regular-svg-icons";
+import { faSquare } from "@fortawesome/free-regular-svg-icons";
 
-import {faPhone, faAt, faUser, faBirthdayCake, faCalendarAlt, faAddressCard,
-	faGraduationCap, faCheckSquare, faKey, faChevronLeft, faExclamationTriangle,
-	faUserPlus, faSignInAlt, faArrowRight, faTimes, faCheckCircle, faTimesCircle,
-	faCircleNotch, faSync} from "@fortawesome/free-solid-svg-icons";
-import {faFacebookSquare} from "@fortawesome/free-brands-svg-icons/faFacebookSquare";
-import {faGoogle} from "@fortawesome/free-brands-svg-icons/faGoogle";
-import { LoginPermissionFailureComponent } from './login-permission-failure/login-permission-failure.component';
-import { LogoutComponent } from './logout/logout.component';
-import {UserDetailEditComponent} from "../user-detail/user-detail-edit/user-detail-edit.component";
-import {BrowserModule} from "@angular/platform-browser";
-import {UserDetailModule} from "../user-detail/user-detail.module";
+import {
+	faPhone,
+	faAt,
+	faUser,
+	faBirthdayCake,
+	faCalendarAlt,
+	faAddressCard,
+	faGraduationCap,
+	faCheckSquare,
+	faKey,
+	faChevronLeft,
+	faExclamationTriangle,
+	faUserPlus,
+	faSignInAlt,
+	faArrowRight,
+	faTimes,
+	faCheckCircle,
+	faTimesCircle,
+	faCircleNotch,
+	faSync
+} from "@fortawesome/free-solid-svg-icons";
+import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons/faFacebookSquare";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons/faGoogle";
+import { LoginPermissionFailureComponent } from "./login-permission-failure/login-permission-failure.component";
+import { LogoutComponent } from "./logout/logout.component";
+import { UserDetailEditComponent } from "../user-detail/user-detail-edit/user-detail-edit.component";
+import { BrowserModule } from "@angular/platform-browser";
+import { UserDetailModule } from "../user-detail/user-detail.module";
 
-
-library.add(faPhone, faTimes, faAt, faUser, faBirthdayCake, faCalendarAlt,
-	faAddressCard, faGraduationCap, faSquare, faCheckSquare, faKey,
-	faFacebookSquare, faGoogle, faChevronLeft, faExclamationTriangle,
-	faUserPlus, faSignInAlt, faArrowRight, faCheckCircle, faTimesCircle,
-	faCircleNotch, faSync);
-
+library.add(
+	faPhone,
+	faTimes,
+	faAt,
+	faUser,
+	faBirthdayCake,
+	faCalendarAlt,
+	faAddressCard,
+	faGraduationCap,
+	faSquare,
+	faCheckSquare,
+	faKey,
+	faFacebookSquare,
+	faGoogle,
+	faChevronLeft,
+	faExclamationTriangle,
+	faUserPlus,
+	faSignInAlt,
+	faArrowRight,
+	faCheckCircle,
+	faTimesCircle,
+	faCircleNotch,
+	faSync
+);
 
 @NgModule({
 	imports: [
@@ -73,10 +110,7 @@ library.add(faPhone, faTimes, faAt, faUser, faBirthdayCake, faCalendarAlt,
 		LoginPermissionFailureComponent,
 		LogoutComponent
 	],
-	exports: [
-		LoginComponent,
-		UserDetailEditComponent
-	]
+	exports: [LoginComponent, UserDetailEditComponent]
 })
 export class LoginModule {
 	public static withConfig(settings?: LoginModuleSettings) {
@@ -86,7 +120,13 @@ export class LoginModule {
 			}
 
 			if (settings.userAgreementUrl) {
-				LOGIN_MODULE_SETTINGS.userAgreementUrl = settings.userAgreementUrl;
+				LOGIN_MODULE_SETTINGS.userAgreementUrl =
+					settings.userAgreementUrl;
+			}
+
+			if (settings.userDetailNotValidPath) {
+				LOGIN_MODULE_SETTINGS.userDetailNotValidPath =
+					settings.userDetailNotValidPath;
 			}
 
 			if (settings.apiPath) {
@@ -98,11 +138,13 @@ export class LoginModule {
 			}
 
 			if (settings.permissionDeniedPath) {
-				LOGIN_MODULE_SETTINGS.permissionDeniedPath = settings.permissionDeniedPath;
+				LOGIN_MODULE_SETTINGS.permissionDeniedPath =
+					settings.permissionDeniedPath;
 			}
 
 			if (settings.registerSuccessPath) {
-				LOGIN_MODULE_SETTINGS.registerSuccessPath = settings.registerSuccessPath;
+				LOGIN_MODULE_SETTINGS.registerSuccessPath =
+					settings.registerSuccessPath;
 			}
 
 			if (settings.permissions) {
