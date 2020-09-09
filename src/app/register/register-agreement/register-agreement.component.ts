2b26077ee8d2c86c4b3f3e0456f0bce7937bf7d0
holskil <aholskil@gmail.com>
2020-09-09 14:26:10 +0200

    fix: must agree to agreement before registering

--- src/app/register/register-agreement/register-agreement.component.ts
+++ src/app/register/register-agreement/register-agreement.component.ts
@@ -1,14 +1,13 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
-import {NgbModal, ModalDismissReasons} from "@ng-bootstrap/ng-bootstrap";
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { NgbModal, ModalDismissReasons } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
-	selector: 'bl-register-agreement',
-	templateUrl: './register-agreement.component.html',
-	styleUrls: ['./register-agreement.component.scss']
+	selector: "bl-register-agreement",
+	templateUrl: "./register-agreement.component.html",
+	styleUrls: ["./register-agreement.component.scss"]
 })
 export class RegisterAgreementComponent implements OnInit {
-
 	@Output() confirmed: EventEmitter<boolean> = new EventEmitter();
 	@Output() dismissed: EventEmitter<boolean> = new EventEmitter();
 
@@ -18,12 +17,12 @@ export class RegisterAgreementComponent implements OnInit {
 
 	constructor(private _modalService: NgbModal) {
 		this.userAgreement = false;
-		this.userAgreementInfoText = 'I have read and understood the user-agreement.';
-		this._closeResult = '';
+		this.userAgreementInfoText =
+			"I have read and understood the user-agreement.";
+		this._closeResult = "";
 	}
 
-	ngOnInit() {
-	}
+	ngOnInit() {}
 
 	public onUserAgreement(content) {
 		if (!this.userAgreement) {
@@ -35,28 +34,33 @@ export class RegisterAgreementComponent implements OnInit {
 	}
 
 	public open(content) {
-		this._modalService.open(content, {centered: true, backdropClass: 'bl-modal'}).result.then((result) => {
-			this._closeResult = 'closed with' + result;
-			this.userAgreement = true;
-			this.confirmed.emit(true);
-		}).catch((reason) => {
-			this._closeResult = 'Dismissed: ' + this.getDismissReason(reason);
-			console.log(this._closeResult);
-			this.userAgreement = false;
-			this.dismissed.emit(true);
-		});
-
+		this._modalService
+			.open(content, {
+				centered: true,
+				backdropClass: "bl-modal",
+				size: "lg"
+			})
+			.result.then(result => {
+				this._closeResult = "closed with" + result;
+				this.userAgreement = true;
+				this.confirmed.emit(true);
+			})
+			.catch(reason => {
+				this._closeResult =
+					"Dismissed: " + this.getDismissReason(reason);
+				console.log(this._closeResult);
+				this.userAgreement = false;
+				this.dismissed.emit(true);
+			});
 	}
 
 	private getDismissReason(reason: any): string {
 		if (reason === ModalDismissReasons.ESC) {
-			return 'by pressing ESC';
+			return "by pressing ESC";
 		} else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-			return 'by clicking on a backdrop';
+			return "by clicking on a backdrop";
 		} else {
-			return  `with: ${reason}`;
+			return `with: ${reason}`;
 		}
 	}
-
-
 }
